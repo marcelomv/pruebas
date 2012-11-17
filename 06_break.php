453991a98c034e86fe4a703635f854dfd061eb08
Marcelo Muñoz pc2 <marcelo@joako-linux.(none)>
2012-11-17 13:12:02 -0300

    ejercicio nº 6

---
+++ 06_break.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<title>06 break</title>
+</head>
+<body>
+<h1>Break en la ventana del navegador</h1>
+
+<p>
+
+<?php
+	$Name="Joaquin";
+	echo "Hola <b>$Name</b>, encantado de conocerte<br>";
+	echo "gracias por venir!!!!!!1"
+
+?>
+
+
+</p>
+
+</body>
+
+
+</html>
